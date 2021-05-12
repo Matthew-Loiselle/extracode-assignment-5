2f5d8817555977429c3f0ac4c7439fc5f394bfc9
MatthewLoiselle <>
2021-05-12 17:57:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import math
+pi = 3.14159
+diameter = float(input("Enter the diameter of the circle:"))
+radius = diameter/ 2
+area = pi* radius * radius
+circumference = pi* 2* radius
+print("Area")
+print("%.5f" %area)
+print("Circumference:")
+print("%.5f" %circumference)
